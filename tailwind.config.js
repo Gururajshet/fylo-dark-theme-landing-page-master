ca9a6419c42ef473d81b343e475d757882e3aa8b
Gururajshet <86518851+Gururajshet@users.noreply.github.com>
2021-10-31 16:42:25 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        darkBlueintro: "hsl(217, 28%, 15%)",
+        darkBlueBack: "hsl(218, 28%, 13%)",
+        darkBlueFooter: "hsl(216, 53%, 9%)",
+        darkBluetestimonial: "hsl(219, 30%, 18%)",
+        CYan: "hsl(176, 68%, 64%)",
+        Bluee: "hsl(198, 60%, 50%)",
+        lightred: "hsl(0, 100%, 63%)",
+      },
+      fontFamily: {
+        Raleway: ["Raleway"],
+        OpenSans: ["Open Sans"],
+      },
+      fontSize: {
+        base: "87.5",
+        primary: "14px",
+      },
+      backgroundImage: {
+        desktop: ' url("../images/bg-curvy-desktop.svg")',
+        mobile: 'url("../images/bg-curvy-mobile.svg")',
+      },
+      spacing: {
+        '18': '74px'
+      }
+    },
+  },
+  variants: {
+    extend: {
+      colors: ['active'],
+      backgroundColor: ['active'],
+      fontWeight: ['focus'],
+    },
+  },
+  plugins: [],
+};
